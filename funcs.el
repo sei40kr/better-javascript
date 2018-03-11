6806c85fa683f80fa7209850554d5a08260363fc
Seong Yong-ju <sei40kr@gmail.com>
2018-03-12 03:05:06 +0900

    Update funcs.el, packages.el

--- funcs.el
+++ funcs.el
@@ -8,13 +8,36 @@
 ;;
 ;;; License: GPLv3
 
-(defun spacemacs//javascript-typescript-setup-lsp ())
+;; Based on https://github.com/an-sh/flow-minor-mode/blob/master/flow-minor-mode.el
+(defun spacemacs//flow-annotation-present-p ()
+  "Detect the '@flow' annotation is present in current buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (let (stop found)
+      (while (not stop)
+        (when (not (re-search-forward "[^\n[:space:]]" nil t))
+          (setq stop t))
+        (backward-char)
+        (cond ((or (looking-at "//+[ ]*@flow")
+                 (looking-at "/\\**[ ]*@flow"))
+                (setq found t)
+                (setq stop t))
+          ((looking-at "//")
+            (forward-line))
+          ((looking-at "/\\*")
+            (when (not (re-search-forward "*/" nil t))
+              (setq stop t)))
+          (t (setq stop t))))
+      found)))
 
-(defun spacemacs//javascript-flow-setup-lsp ())
+(defun spacemacs//javascript-setup-backend ()
+  "Conditionally setup javascript backend."
+  (spacemacs//javascript-setup-lsp))
 
-(defun spacemacs//javascript-setup-lsp())
+(defun spacemacs//javascript-setup-company ()
+  "Conditionally setup company based on backend."
+  (spacemacs//javascript-setup-lsp-company))
 
-(defun spacemacs//javascript-setup-lsp-company())
 
 ;; import-js
 
@@ -25,14 +48,52 @@
       (spacemacs-buffer/warning "importjsd binary not found!"))
     found))
 
+
+;; lsp
+
+(defun spacemacs//javascript-setup-lsp ()
+  "Setup lsp backend."
+  (if (configuration-layer/layer-used-p 'lsp)
+    (if (and
+          (configuration-layer/package-used-p 'lsp-javascript-flow)
+          (spacemacs//flow-annotation-present-p))
+      (progn
+        (require 'lsp-javascript-flow)
+        (lsp-javascript-flow-enable))
+      (if (configuration-layer/package-used-p 'lsp-typescript)
+        (progn
+          (require 'lsp-typescript)
+          (lsp-typescript-enable))))
+    (message "`lsp' layer is not installed, please add `lsp' layer to your dofile.")))
+
+(defun spacemacs//javascript-setup-lsp-company ()
+  "Setup lsp auto-completion."
+  (if (configuration-layer/layer-used-p 'lsp)
+    (progn
+      (spacemacs|add-company-backends
+        :backends company-lsp
+        :modes rjsx-mode)
+      (company-mode))
+    (message "`lsp' layer is not installed, please add `lsp' layer to your dofile.")))
+
+
 ;; rjsx-mode
 
 (defun spacemacs//setup-rjsx-mode ()
+  ;; See https://github.com/syl20bnr/spacemacs/issues/8222
+  (setq-local company-minimum-prefix-length 2)
   ;; See https://github.com/CestDiego/emmet-mode/commit/3f2904196e856d31b9c95794d2682c4c7365db23
   (setq-local emmet-expand-jsx-className? t)
+  ;; lsp show parse errors on its own
+  (setq-local js2-mode-show-parse-errors nil)
+  (setq-local js2-mode-assume-strict nil)
+  (setq-local js2-mode-show-strict-warnings nil)
+  (setq-local js2-strict-trailing-comma-warning nil)
+  (setq-local js2-strict-missing-semi-warning nil)
+  (setq-local js2-missing-semi-one-line-override t)
+  (setq-local js2-strict-inconsistent-return-warning nil)
+  (setq-local js2-strict-cond-assign-warning nil)
+  (setq-local js2-strict-var-redeclaration-warning nil)
+  (setq-local js2-strict-var-hides-function-arg-warning nil)
   ;; Enable js-mode snippets
-  (yas-activate-extra-mode 'js-mode)
-  ;; Don't auto-quote attribute values
-  (setq-local web-mode-enable-auto-quoting nil)
-  ;; See https://github.com/syl20bnr/spacemacs/issues/8222
-  (set (make-local-variable 'company-minimum-prefix-length) 2))
+  (yas-activate-extra-mode 'js-mode))
--- packages.el
+++ packages.el
@@ -9,34 +9,44 @@
 ;;; License: GPLv3
 
 (setq better-javascript-packages
-      '(
-        add-node-modules-path
-        emmet-mode
-        evil-matchit
-        flycheck
-        (import-js :toggle (spacemacs//import-js-detect))
-        js-doc
-        js2-refactor
-        livid-mode
-        rjsx-mode
-        skewer-mode
-        smartparens))
+  '(
+     add-node-modules-path
+     company
+     emmet-mode
+     evil-matchit
+     flycheck
+     (import-js :toggle (spacemacs//import-js-detect))
+     js-doc
+     livid-mode
+     lsp-mode
+     (lsp-javascript-flow :location local)
+     (lsp-typescript :location local)
+     rjsx-mode
+     skewer-mode
+     smartparens))
 
-(setq better-javascript-excluded-packages '(company-tern tern web-beautify))
+(setq better-javascript-excluded-packages
+  '(
+     company-tern
+     tern
+     web-beautify))
 
 (defun better-javascript/post-init-add-node-modules-path ()
   (add-hook 'rjsx-mode-hook #'add-node-modules-path))
 
+(defun better-javascript/post-init-company ()
+  (add-hook 'rjsx-mode-hook #'spacemacs//javascript-setup-lsp-company))
+
 (defun better-javascript/post-init-emmet-mode ()
-  (add-hook 'rjsx-mode-hook 'emmet-mode))
+  (add-hook 'rjsx-mode-hook #'emmet-mode))
 
 (defun better-javascript/post-init-evil-matchit ()
   (with-eval-after-load 'evil-matchit
     (plist-put evilmi-plugins 'rjsx-mode
-               '((evilmi-simple-get-tag evilmi-simple-jump)
-                 (evilmi-javascript-get-tag evilmi-javascript-jump)
-                 (evilmi-html-get-tag evilmi-html-jump))))
-  (add-hook `rjsx-mode `turn-on-evil-matchit-mode))
+      '((evilmi-simple-get-tag evilmi-simple-jump)
+         (evilmi-javascript-get-tag evilmi-javascript-jump)
+         (evilmi-html-get-tag evilmi-html-jump))))
+  (add-hook 'rjsx-mode-hook #'turn-on-evil-matchit-mode))
 
 (defun better-javascript/post-init-flycheck ()
   (dolist (mode '(rjsx-mode json-mode))
@@ -54,59 +64,8 @@
         "ig" 'import-js-goto))))
 
 (defun better-javascript/post-init-js-doc ()
-  (add-hook 'rjsx-mode-hook 'spacemacs/js-doc-require)
+  (add-hook 'rjsx-mode-hook #'spacemacs/js-doc-require)
   (spacemacs/js-doc-set-key-bindings 'rjsx-mode))
-
-(defun better-javascript/pre-init-js2-refactor ()
-  (add-hook 'rjsx-mode-hook 'spacemacs/js2-refactor-require)
-  ;; prefixes
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mr3" "ternary")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mra" "add/args")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrb" "barf")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrc" "contract")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mre" "expand/extract")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mri" "inline/inject/introduct")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrl" "localize/log")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrr" "rename")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrs" "split/slurp")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrt" "toggle")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mru" "unwrap")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrv" "var")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mrw" "wrap")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mx" "text")
-  (spacemacs/declare-prefix-for-mode 'rjsx-mode "mxm" "move")
-  ;; key bindings
-  (spacemacs/set-leader-keys-for-major-mode 'rjsx-mode
-    "r3i" 'js2r-ternary-to-if
-    "rag" 'js2r-add-to-globals-annotation
-    "rao" 'js2r-arguments-to-object
-    "rba" 'js2r-forward-barf
-    "rca" 'js2r-contract-array
-    "rco" 'js2r-contract-object
-    "rcu" 'js2r-contract-function
-    "rea" 'js2r-expand-array
-    "ref" 'js2r-extract-function
-    "rem" 'js2r-extract-method
-    "reo" 'js2r-expand-object
-    "reu" 'js2r-expand-function
-    "rev" 'js2r-extract-var
-    "rig" 'js2r-inject-global-in-iife
-    "rip" 'js2r-introduce-parameter
-    "riv" 'js2r-inline-var
-    "rlp" 'js2r-localize-parameter
-    "rlt" 'js2r-log-this
-    "rrv" 'js2r-rename-var
-    "rsl" 'js2r-forward-slurp
-    "rss" 'js2r-split-string
-    "rsv" 'js2r-split-var-declaration
-    "rtf" 'js2r-toggle-function-expression-and-declaration
-    "ruw" 'js2r-unwrap
-    "rvt" 'js2r-var-to-this
-    "rwi" 'js2r-wrap-buffer-in-iife
-    "rwl" 'js2r-wrap-in-for-loop
-    "k" 'js2r-kill
-    "xmj" 'js2r-move-line-down
-    "xmk" 'js2r-move-line-up))
 
 (defun better-javascript/pre-init-livid-mode ()
   (spacemacs|add-toggle javascript-repl-live-evaluation
@@ -114,24 +73,30 @@
     :documentation "Live evaluation of JS buffer change."
     :evil-leader-for-mode (rjsx-mode . "Tl")))
 
+(defun better-javascript/pre-init-lsp-mode ()
+  (add-hook 'rjsx-mode-hook #'spacemacs//javascript-setup-lsp))
+
+(defun better-javascript/init-lsp-javascript-flow ()
+  (use-package lsp-javascript-flow
+    :commands lsp-javascript-flow-enable))
+
+(defun better-javascript/init-lsp-typescript ()
+  (use-package lsp-typescript
+    :commands lsp-typescript-enable))
+
 (defun better-javascript/init-rjsx-mode ()
   (use-package rjsx-mode
     :defer t
     :init
     (progn
-      (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
-      (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
-      (add-hook 'rjsx-mode-hook 'spacemacs//setup-rjsx-mode))
+      (add-to-list 'auto-mode-alist '("\\.js[x]?$" . rjsx-mode))
+      (add-hook 'rjsx-mode-hook #'spacemacs//setup-rjsx-mode))
     :config
     (progn
       ;; prefixes
-      (spacemacs/declare-prefix-for-mode 'rjsx-mode "mh" "documentation")
-      (spacemacs/declare-prefix-for-mode 'rjsx-mode "mg" "goto")
-      (spacemacs/declare-prefix-for-mode 'rjsx-mode "mr" "refactor")
       (spacemacs/declare-prefix-for-mode 'rjsx-mode "mz" "folding")
       ;; key bindings
       (spacemacs/set-leader-keys-for-major-mode 'rjsx-mode
-        "w" 'js2-mode-toggle-warnings-and-errors
         "zc" 'js2-mode-hide-element
         "zo" 'js2-mode-show-element
         "zr" 'js2-mode-show-all
@@ -140,7 +105,7 @@
         "zC" 'js2-mode-toggle-hide-comments))))
 
 (defun better-javascript/pre-init-skewer-mode ()
-  (add-hook 'rjsx-mode-hook 'skewer-mode))
+  (add-hook 'rjsx-mode-hook #'skewer-mode))
 
 (defun better-javascript/post-init-skewer-mode ()
   (spacemacs/declare-prefix-for-mode 'rjsx-mode "ms" "skewer")
@@ -160,5 +125,5 @@
 
 (defun better-javascript/post-init-smartparens ()
   (if dotspacemacs-smartparens-strict-mode
-      (add-hook 'rjsx-mode-hook #'smartparens-strict-mode)
+    (add-hook 'rjsx-mode-hook #'smartparens-strict-mode)
     (add-hook 'rjsx-mode-hook #'smartparens-mode)))
